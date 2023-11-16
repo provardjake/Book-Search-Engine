846f27f7903b858a48c186d9c037ec4f6dd19302
Jake Provard <provardjake@gmail.com>
2023-11-15 20:15:54 -0700

    updated searchBooks.jsx to use useMutation

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -7,9 +7,10 @@ import {
   Card,
   Row
 } from 'react-bootstrap';
-
+import {useMutation} from "@apollo/client";
+import {SAVE_BOOK} from "../utils/mutations";
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -20,6 +21,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, {error}] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -72,7 +75,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: {bookData: bookToSave}
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
