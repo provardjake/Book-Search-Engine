168769af001bcf6ab947c8233749c57ad7931fcc
Jake Provard <provardjake@gmail.com>
2023-11-24 19:33:32 -0700

    fixed bug with getting the users saved books

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,19 @@
 import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
-    query user($username: String!){
-        user(username: $username){
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
-    }`;
+    query me {
+    me {
+        _id
+        bookCount
+        email
+        username
+        savedBooks {
+            authors
+            bookId
+            description
+            image
+            link
+            title
+    }
+  }
+}`;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const resolvers = {
     Query: {
         me: async(parent, args, context) =>{
             if(context.user){
-                return User.findOne({_id: context.user._id}).populate("books");
+                return await User.findOne({_id: context.user._id}).populate("savedBooks");
             }
             throw AuthenticationError;
         }
